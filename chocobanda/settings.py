ef04355e676c28a7f7be1f5d605537640a1ce765
AntoVGreco <antonelagreco@gmail.com>
2024-09-08 21:39:21 -0300

    feat base.html
    creamos el archivo base.html y lo conectamos al sitio utilizando las herramientas de django

--- chocobanda/settings.py
+++ chocobanda/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import sitioChocobanda
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sitioChocobanda',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'chocobanda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'sitioChocobanda', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
