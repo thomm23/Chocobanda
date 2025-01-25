fabcc80f8322b2c8c1ce918b3a97e3820da44a00
garciapatricio <garciapatricio1@gmail.com>
2025-01-25 14:15:45 -0300

    UPDATE se agrego que la galeriaObra se pueda cargar directamente desde el panel de alta de obra en admin

--- sitioChocobanda/admin.py
+++ sitioChocobanda/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
-
+# Importa tus modelos
 from .models import Integrante, Personaje, Obra, GaleriaObra, Evento, Noticia, Institucion, Multimedia, Galeria, GaleriaInstitucion, Cancion, Video, AjustesPagina
 
+# Registrar los modelos en el admin
 admin.site.register(Integrante)
 admin.site.register(Personaje)
-admin.site.register(Obra)
-admin.site.register(GaleriaObra)
 admin.site.register(Evento)
 admin.site.register(Noticia)
 admin.site.register(Institucion)
@@ -17,3 +15,18 @@ admin.site.register(GaleriaInstitucion)
 admin.site.register(Cancion)
 admin.site.register(Video)
 admin.site.register(AjustesPagina)
+
+# Clase para el Inline de GaleriaObra
+class GaleriaObraInline(admin.TabularInline):
+    model = GaleriaObra
+    extra = 1  # Para agregar un formulario vacío para una nueva imagen
+    fields = ['foto', 'descripcion']
+    verbose_name = "Imagen de la galería"
+    verbose_name_plural = "Galería de la obra"
+
+# Registrar el modelo Obra con su clase personalizada de admin
+@admin.register(Obra) 
+class ObraAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descripcion')
+    search_fields = ('titulo',)
+    inlines = [GaleriaObraInline]
